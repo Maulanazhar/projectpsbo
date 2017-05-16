2f53e5cfbc68d8a5582e7520a39eac8afcf3a7a2
Maulanazhar <Maulanazhar@users.noreply.github.com>
2017-05-16 16:40:03 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,32 @@
+<div id="footerwrap">
+	 	<div class="container">
+		 	<div class="row">
+		 		<div class="col-lg-4">
+		 			<h4>About</h4>
+		 			<div class="hline-w"></div>
+		 			<p>Pinjam.In adalah e-commerce yang bertujuan untuk membantu konsumen dalam memilih barang pilihan mereka dengan cara meminjam</p>
+		 		</div>
+		 		<div class="col-lg-4">
+		 			<h4>Social Media</h4>
+		 			<div class="hline-w"></div>
+		 			<p>
+		 				<a href="#"><i class="fa fa-dribbble"></i></a>
+		 				<a href="#"><i class="fa fa-facebook"></i></a>
+		 				<a href="#"><i class="fa fa-twitter"></i></a>
+		 				<a href="#"><i class="fa fa-instagram"></i></a>
+		 				<a href="#"><i class="fa fa-tumblr"></i></a>
+		 			</p>
+		 		</div>
+		 		<div class="col-lg-4">
+		 			<h4>Address</h4>
+		 			<div class="hline-w"></div>
+		 			<p>
+		 				Jalan Meranti FMIPA IPB<br/>
+		 				Institut Pertanian Bogor<br/>
+		 				Indonesia<br/>
+		 			</p>
+		 		</div>
+		 	
+		 	</div><! --/row -->
+	 	</div><! --/container -->
+	 </div><! --/footerwrap -->
